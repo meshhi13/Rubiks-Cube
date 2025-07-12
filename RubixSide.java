d6b7fad264ce29775a0f0bae53fb3a80774563c1
Himesh Ahuja <hhhimesh.131@gmail.com>
2025-07-12 19:06:13 -0400

    adding rubix side

---
+++ RubixSide.java
@@ -0,0 +1,54 @@
+package Rubix;
+
+import javafx.scene.paint.Color;
+
+public class RubixSide {
+    static final int SIZE = 3;
+    Color[][] values = new Color[SIZE][SIZE];
+
+    public RubixSide(Color color) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                values[i][j] = color;
+            }
+        }
+    }
+
+    public Color[][] transposeSide(){
+        int m = values.length;
+        int n = values[0].length;
+
+        Color[][] transposeSide = new Color[n][m];
+
+        for(int x = 0; x < n; x++) {
+            for(int y = 0; y < m; y++) {
+                transposeSide[x][y] = values[y][x];
+            }
+        }
+
+        return transposeSide;
+    }
+
+    public Color[] getRow(int row){
+        Color[] getRow = new Color[SIZE];
+        for (int i = 0; i < SIZE; i++){
+            getRow[i] = values[i][row];
+        }
+
+        return getRow;
+    }
+
+    public Color[] getCol(int col){
+        return values[col];
+    }
+
+    public void setRow(int row, Color[] newValues){
+        for (int i = 0; i < SIZE; i++){
+            values[i][row] = newValues[i];
+        }
+    }
+
+    public void setCol(int col, Color[] newValues){
+        values[col] = newValues;
+    }
+}
